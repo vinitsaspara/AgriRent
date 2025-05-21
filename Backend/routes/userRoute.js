1e6dac144de822a89b0cd8c9643e46dc7a6458bd
sasparavinit@gmail.com <sasparavinit@gmail.com>
2025-05-21 18:04:18 +0530

    add changes of the navbar and logout

--- Backend/routes/userRoute.js
+++ Backend/routes/userRoute.js
@@ -1,10 +1,11 @@
 import express from "express"
 import { login, logout, signUp } from "../controllers/user.controller.js";
+import authMiddleware from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
 router.route("/signup").post(signUp);
 router.route("/login").post(login);
-router.route("/logout").post(logout);
+router.route("/logout").get(authMiddleware,logout);
 
 export default router;
