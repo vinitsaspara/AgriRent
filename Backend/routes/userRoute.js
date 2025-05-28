181b3198b03ac7d1f6489915de66ad0d6e6975cb
sasparavinit@gmail.com <sasparavinit@gmail.com>
2025-05-28 19:05:06 +0530

    frontend profile update complete

--- Backend/routes/userRoute.js
+++ Backend/routes/userRoute.js
@@ -1,6 +1,7 @@
 import express from "express"
 import { deleteMember, getAllUsers, login, logout, signUp, updateMember, updateProfile } from "../controllers/user.controller.js";
 import authMiddleware from "../middlewares/authMiddleware.js";
+import { singleUpload } from "../middlewares/multer.js";
 
 const router = express.Router();
 
@@ -10,6 +11,6 @@ router.route("/logout").get(authMiddleware,logout);
 router.route("/get-all-user").get(authMiddleware,getAllUsers);
 router.route("/update-member/:id").put(authMiddleware,updateMember);
 router.route("/delete-member/:id").delete(authMiddleware, deleteMember);
-router.route("/update-user-profile").put(authMiddleware,updateProfile);
+router.route("/update-user-profile").put(authMiddleware,singleUpload,updateProfile);
 
 export default router;
